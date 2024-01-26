f814d2ff22dafa36b486c61e2243fa2f0df64d90
pietrostraneo <straneo_p@yahoo.it>
2024-01-26 18:01:43 +0100

    Modified some positions in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <footer class="bg-dark text-white p-3">
     <div class="container-fluid">
-        <div class="row d-flex justify-content-between">
+        <div class="row d-flex justify-content-between align-items-center">
             <div class="col-4">
                 <h2>BoolHotels</h2>
                 <p>Class #113</p>
@@ -10,9 +10,9 @@
                     <li>
                         <b class="text-success">Address:</b>
                         <p>
-                            Apple Inc.
-                            1 Apple Park Way
-                            Cupertino, CA 95014
+                            Apple Inc. <br>
+                            1 Apple Park Way <br>
+                            Cupertino, CA 95014 <br>
                             United States
                         </p>
                     </li>
